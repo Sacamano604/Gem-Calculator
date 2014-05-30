788c88d2d826d7888abb6d12076797a55d519331
Ben MacBook <info@van-markruudnew.jordancap.com>
2014-05-30 06:35:20 -0700

    Drop down working
    Able to pass information from the drop down to the js file. Committing
    now as a working function more to come…

--- js/calc.js
+++ js/calc.js
@@ -1,11 +1,12 @@
 
 
 var impEmeraldCalc = function(quantity, type) {
+	console.log(type);
 	if (isNaN(quantity)){
 		document.getElementById('results').innerHTML = "<p>Sorry please enter a valid number</p>";
 	} else {
 		var val = quantity * 3;
-		document.getElementById('results').innerHTML = "<p>You need " + val + " Marquise Emeralds to create " + quantity + " Imperial Emeralds</p>";
+		document.getElementById('results').innerHTML = "<p>You need " + val + " Marquise Emeralds to create " + quantity + " Imperial Emeralds</p>" + type;
 	}
 }
 
