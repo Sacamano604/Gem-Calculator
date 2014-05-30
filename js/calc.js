cd8ba84cb3f401b079cfa51abbf28feac37c0ede
Ben MacBook <info@van-markruudnew.jordancap.com>
2014-05-30 07:26:29 -0700

    'Cut Type' Dropdown working
    Math and drop down working, need gem type on next commit

--- js/calc.js
+++ js/calc.js
@@ -1,12 +1,22 @@
 
 
 var impEmeraldCalc = function(quantity, type) {
-	console.log(type);
 	if (isNaN(quantity)){
-		document.getElementById('results').innerHTML = "<p>Sorry please enter a valid number</p>";
+		document.getElementById('results').innerHTML = "<p id='error'>Please enter a valid number</p>";
 	} else {
-		var val = quantity * 3;
-		document.getElementById('results').innerHTML = "<p>You need " + val + " Marquise Emeralds to create " + quantity + " Imperial Emeralds</p>" + type;
+		  	if (type === "imperial") {
+				var val = quantity * 3;
+				document.getElementById('results').innerHTML = "<p>You need " + val + " Marquise to create " + quantity + " Imperial</p>";	
+			} else if (type === "flawlessimperial") {
+				var val = quantity * 9;
+				document.getElementById('results').innerHTML = "<p>You need " + val + " Marquise to create " + quantity + " Flawless Imperial</p>";
+			} else if (type === "royal") {
+				var val = quantity * 27;
+				document.getElementById('results').innerHTML = "<p>You need " + val + " Marquise to create " + quantity + " Royal</p>" 
+			} else if (type === "flawlessroyal") {
+				var val = quantity * 81;
+				document.getElementById('results').innerHTML = "<p>You need " + val + " Marquise to create " + quantity + " Flawless Royal</p>"
+			}
 	}
 }
 
