25d1801929b0bd780124e7eff55ebf426742e046
Ben MacBook <info@Bens-MacBook-Air.local>
2014-05-29 21:24:13 -0700

    Adding drop down items

--- js/calc.js
+++ js/calc.js
@@ -1,11 +1,11 @@
 
 
-var impEmeraldCalc = function(num) {
-	if (isNaN(num)){
+var impEmeraldCalc = function(quantity, type) {
+	if (isNaN(quantity)){
 		document.getElementById('results').innerHTML = "<p>Sorry please enter a valid number</p>";
 	} else {
-		var val = num * 3;
-		document.getElementById('results').innerHTML = "<p>You need " + val + " Marquise Emeralds to create " + num + " Imperial Emeralds</p>";
+		var val = quantity * 3;
+		document.getElementById('results').innerHTML = "<p>You need " + val + " Marquise Emeralds to create " + quantity + " Imperial Emeralds</p>";
 	}
 }
 
