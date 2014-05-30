e7d398b01b0bed1c78ba31426f78833b56e02f7f
Ben MacBook <info@van-markruudnew.jordancap.com>
2014-05-30 08:39:59 -0700

    Everything functioning, v2

--- js/calc.js
+++ js/calc.js
@@ -1,21 +1,21 @@
 
 
-var impEmeraldCalc = function(quantity, type) {
+var impEmeraldCalc = function(quantity, gemKind, type) {
 	if (isNaN(quantity)){
 		document.getElementById('results').innerHTML = "<p id='error'>Please enter a valid number</p>";
 	} else {
 		  	if (type === "imperial") {
 				var val = quantity * 3;
-				document.getElementById('results').innerHTML = "<p>You need " + val + " Marquise to create " + quantity + " Imperial</p>";	
+				document.getElementById('results').innerHTML = "<p>You need " + val + " Marquise to create " + quantity + " Imperial " + gemKind;	
 			} else if (type === "flawlessimperial") {
 				var val = quantity * 9;
-				document.getElementById('results').innerHTML = "<p>You need " + val + " Marquise to create " + quantity + " Flawless Imperial</p>";
+				document.getElementById('results').innerHTML = "<p>You need " + val + " Marquise to create " + quantity + " Flawless Imperial " + gemKind;
 			} else if (type === "royal") {
 				var val = quantity * 27;
-				document.getElementById('results').innerHTML = "<p>You need " + val + " Marquise to create " + quantity + " Royal</p>" 
+				document.getElementById('results').innerHTML = "<p>You need " + val + " Marquise to create " + quantity + " Royal " + gemKind; 
 			} else if (type === "flawlessroyal") {
 				var val = quantity * 81;
-				document.getElementById('results').innerHTML = "<p>You need " + val + " Marquise to create " + quantity + " Flawless Royal</p>"
+				document.getElementById('results').innerHTML = "<p>You need " + val + " Marquise to create " + quantity + " Flawless Royal " + gemKind;
 			}
 	}
 }
