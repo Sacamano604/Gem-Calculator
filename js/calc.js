8b3d5d6982b4772a8e6b60b5310d29ad094ae967
Ben MacBook <info@Bens-MacBook-Air.local>
2014-06-03 19:49:10 -0700

    From if/else to switch
    Changed the if/else statement to a switch, although the output is
    coming in backwards. Will need to fix before hard Version 3. Also
    applied some styles and an output for the user’s selection.

--- js/calc.js
+++ js/calc.js
@@ -1,21 +1,35 @@
-var gemCalculator = function(quantity, gemType){
+var gemCalculator = function(quantity, gemType, gemKind){
 	// Checks to make sure if the quantity entered is a number or not.
 	if (isNaN(quantity)){
 		document.getElementById('errorMessage').innerHTML = "<p id='error'>Please enter a valid number</p>";
 	} else {
 
+	document.getElementById('selectionOutput').innerHTML = "<h4 id='warning'>To create " + quantity + " " + gemType + " " + gemKind + " you will need:</h4>";
+	
 	function imperialCalc(quantity){
 		var impQuantity = quantity * 3;
 		document.getElementById("imperialOutput").className = "";
 		document.getElementById('imperialResults').innerHTML = impQuantity;
+		return impQuantity;
 	}
 	function flawlessImperialCalc(quantity){
 		var flawImpQuantity = imperialCalc(quantity) * 3;
 		document.getElementById("flawlessImperialOutput").className = "";
 		document.getElementById('flawlessImperialResults').innerHTML = flawImpQuantity;
+		return flawImpQuantity;
 	}
-
-
+	function royalCalc(quantity){
+		var royalQuantity = flawlessImperialCalc(quantity) * 3;
+		document.getElementById("royalOutput").className = "";
+		document.getElementById('royalResults').innerHTML = royalQuantity;
+		return royalQuantity;
+	}
+	function flawlessRoyalCalc(quantity){
+		var flawlessRoyalQuantity = royalCalc(quantity) * 3;
+		document.getElementById("flawlessRoyalOutput").className = "";
+		document.getElementById('flawlessRoyalResults').innerHTML = flawlessRoyalQuantity;
+		return flawlessRoyalQuantity;
+	}
 
 	switch (gemType){
 		case "imperial":
@@ -23,10 +37,16 @@ var gemCalculator = function(quantity, gemType){
 		break;
 
 		case "flawlessimperial":
-		imperialCalc(quantity);
 		flawlessImperialCalc(quantity);
 		break;
-
+		
+		case "royal":
+		royalCalc(quantity);
+		break;
+		
+		case "flawlessroyal":
+		flawlessRoyalCalc(quantity);
+		break;
 		//Default not nessecary being that the values are choosen from a drop down 
 		default:
 		document.getElementById('errorMessage').innerHTML = "<p id='error'>You have not choosen a valid gemtype</p>";
